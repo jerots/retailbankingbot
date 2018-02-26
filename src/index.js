165f9c2f8c01810c8ee7fa18f9536d50fbb7a94b
ONG TEE SENG, JEREMY _ <jeremyong.2014@smu.edu.sg>
2018-02-26 19:07:39 +0800

    add CORS

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ const expressVueMiddleware = expressVue.init();
 
 app.use(expressVueMiddleware);
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
 })
